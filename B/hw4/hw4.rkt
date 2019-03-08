2665f603f301ce19568f06101db475b1fadb74cc
Andrey Lukin <aimm.legate@gmail.com>
2019-03-08 19:25:53 +0500

    hw4 5

--- B/hw4/hw4.rkt
+++ B/hw4/hw4.rkt
@@ -20,8 +20,23 @@
 
 ;;3
 (define (list-nth-mod xs n)
-  (define (get-n-element list, n) (car (list-tail list n)))
+  (define (get-n-element list n) (car (list-tail list n)))
   (cond
     [(< n 0) (error "list-nth-mod: negative number")]
     [(= (length xs) 0)(error "list-nth-mod: empty list") ]
     [#t (get-n-element xs (remainder n (length xs)))]))
+
+;;4
+(define (stream-for-n-steps s n)
+  (define (aux acc stream)
+    (if (= (length acc) n)
+        acc
+        (aux (cons (car (stream)) acc) (cdr (stream)))))
+  (reverse (aux (list) s)))
+
+;;5
+(define funny-number-stream
+  (letrec ([f (lambda (x)
+                (cons x (lambda () (f (let ([next (+ (abs x) 1)]) (if (= (remainder next 5) 0) (- 0 next) next))))))])
+    (lambda () (f 1))))
+  
