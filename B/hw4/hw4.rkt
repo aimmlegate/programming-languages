aa16d3adc1da0b783fa0d82bb6962b808af43459
Andrey Lukin <aimm.legate@gmail.com>
2019-03-10 13:33:40 +0500

    format-code

--- B/hw4/hw4.rkt
+++ B/hw4/hw4.rkt
@@ -16,7 +16,7 @@
   
 ;;2
 (define (string-append-map xs suffix)
-  (map (lambda (x) (string-append x suffix)) xs))
+  (map (λ (x) (string-append x suffix)) xs))
 
 ;;3
 (define (list-nth-mod xs n)
@@ -36,37 +36,45 @@
 
 ;;5
 (define funny-number-stream
-  (letrec ([f (lambda (x)
-                (cons x (lambda () (f
-                                    (let ([next (+ (abs x) 1)])
-                                      (if (= (remainder next 5) 0) (- 0 next) next))))))])
+  (letrec ([f (λ (x)
+                (cons x
+                      (λ () (f (let ([next (+ (abs x) 1)])
+                                 (if (= (remainder next 5) 0)
+                                     (- 0 next)
+                                     next))))))])
     (lambda () (f 1))))
 
 ;;6
 (define dan-then-dog
   (letrec ([dan "dan.jpg"]
            [dog "dog.jpg"]
-           [f (lambda (str) (cons str (lambda () (f (if (string=? str dog) dan dog)))))])
-  (lambda () (f dan))))
+           [f (λ (str) (cons str (λ () (f (if (string=? str dog) dan dog)))))])
+  (λ () (f dan))))
  
 ;;7
 (define (stream-add-zero stream)
-  (letrec ([f (lambda (s)
+  (letrec ([f (λ (s)
                 (let ([stream-val (car (s))]
                       [stream-fn (cdr (s))])
-                  (cons (cons 0 stream-val) (lambda () (f stream-fn)))))])
+                  (cons (cons 0 stream-val) (λ () (f stream-fn)))))])
   (lambda () (f stream))))
 
 ;;8
 (define (make-stream list)
-  (letrec ([f (lambda (l)
+  (letrec ([f (λ (l)
                 (cons
-                 (if (null? l) (car list) (car l))
-                 (lambda () (f (if (null? l) (cdr list) (cdr l))))))])
-    (lambda() (f list))))
+                 (if (null? l)
+                     (car list)
+                     (car l))
+                 (λ () (f (if (null? l)
+                              (cdr list)
+                              (cdr l))))))])
+    (λ () (f list))))
 
 (define (cycle-lists xs ys)
-  (letrec ([f (lambda (stream-xs stream-ys)
+  (letrec ([f (λ (stream-xs stream-ys)
                (cons (cons (car (stream-xs)) (car (stream-ys)))
-                     (lambda () (f (cdr (stream-xs)) (cdr (stream-ys))))))])
-    (lambda () (f (make-stream xs) (make-stream ys)))))
+                     (λ () (f (cdr (stream-xs)) (cdr (stream-ys))))))])
+    (λ () (f (make-stream xs) (make-stream ys)))))
+
+;;9
