6eeb59676d152dd58752db4c917ea09a5ff3aae8
Andrey Lukin <aimm.legate@gmail.com>
2019-03-23 17:09:41 +0500

    2

--- C/hw6/hw6assignment.rb
+++ C/hw6/hw6assignment.rb
@@ -5,17 +5,45 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
+  All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+                  rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+                  [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+                  [[0, 0], [0, -1], [0, 1], [0, 2]]],
+                  rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+                  rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+                  rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+                  rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]),
+                  rotations([[0, 0], [-1, 0], [1,0], [-1, 1], [0, 1]]),
+                  [[[0, 0], [-1, 0], [1, 0], [2, 0], [3, 0]], # longest (only needs two)
+                  [[0, 0], [0, -1], [0, 1], [0, 2], [0, 3]]],
+                  rotations([[0, 0], [1, 0], [0,1]])]
+
+  def initialize(point_array, board)
+    super(point_array, board)
+  end
 
   # your enhancements here
+  def self.next_piece (board)
+    MyPiece.new(All_My_Pieces.sample, board)
+  end
 
 end
 
 class MyBoard < Board
   # your enhancements here
+  def initialize (game)
+    super (game)
+    @current_block = MyPiece.next_piece(self)
+  end
 
   def rotate_180
     rotate_clockwise
     rotate_clockwise
+  end
+
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
   end
 
 
