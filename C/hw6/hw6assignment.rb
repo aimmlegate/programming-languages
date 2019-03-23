6994e81eba92b279c21e2ae7cc0a4ecccb651702
Andrey Lukin <aimm.legate@gmail.com>
2019-03-24 00:12:11 +0500

    fices

--- C/hw6/hw6assignment.rb
+++ C/hw6/hw6assignment.rb
@@ -12,26 +12,25 @@ class MyPiece < Piece
                   rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
                   rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
                   rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-                  rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]),
-                  rotations([[0, 0], [-1, 0], [1,0], [-1, 1], [0, 1]]),
-                  [[[0, 0], [-1, 0], [1, 0], [2, 0], [3, 0]], # longest (only needs two)
-                  [[0, 0], [0, -1], [0, 1], [0, 2], [0, 3]]],
-                  rotations([[0, 0], [1, 0], [0, 1], [0, 0]])]
+                  rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
+                  rotations([[0, 0], [-1, 0], [1, 0], [0, -1], [1, -1]]),
+                  rotations([[0, 0], [0, 1], [0, -1], [0, -2], [0, 2]]),
+                  rotations([[0, 0], [1, 0], [0, -1]])]
 
-  Cheat_Piece = [[[0, 0], [0, 0], [0, 0], [0, 0]]]
+  Cheat_Piece = [[[0, 0]]]
 
   def initialize(point_array, board)
     super(point_array, board)
   end
 
   # your enhancements here
-  def self.next_piece (board, cheat_status = false)
-    if cheat_status
-      MyPiece.new(Cheat_Piece, board)
-    else
-      MyPiece.new(All_My_Pieces.sample, board)
-    end
+  def self.next_piece (board)
+    MyPiece.new(All_My_Pieces.sample, board)
   end
+
+  def self.cheat_piece (board)
+    MyPiece.new(Cheat_Piece, board)
+  end   
 
 end
 
@@ -49,9 +48,14 @@ class MyBoard < Board
   end
 
   def next_piece
-    @current_block = MyPiece.next_piece(self, @cheat_status)
-    @current_pos = nil
-    @cheat_status = false
+    if @cheat_status 
+      @current_block = MyPiece.cheat_piece(self)
+      @cheat_status = false
+      @current_pos = nil
+    else
+      @current_block = MyPiece.next_piece(self)
+      @current_pos = nil
+    end
   end
 
   def commit_cheat
@@ -61,7 +65,18 @@ class MyBoard < Board
     end
   end
 
-
+  def store_current
+    locations = @current_block.current_rotation
+    l = locations.length - 1
+    displacement = @current_block.position
+    (0..l).each{|index| 
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
 
 
 
